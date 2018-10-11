41713e2e2b690f8391849495ebc5e14a6bfe3db9
lxz <806528479@qq.com>
2018-10-11 21:52:00 +0800

    在原来的基础上增加了倒金字塔的输出

--- PyramidDemo.java
+++ PyramidDemo.java
@@ -5,6 +5,13 @@ git commit -m "first commit"
 git remote add origin https://github.com/esllovelxz/test.git
 git push -u origin master
 echo "# test" >> README.md
+git ini"first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
 git init
 git add README.md
 git commit -m "first commit"
@@ -16,9 +23,38 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/esllovelxz/test.git
 git push -u origin master
+"first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
 echo "# test" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
+"first commit"
 git remote add origin https://github.com/esllovelxz/test.git
 git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+"first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+"first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+"first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
