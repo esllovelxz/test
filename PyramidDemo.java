08499873e81d7c23b38310f34b18a2f6286c5417
lxz <806528479@qq.com>
2018-10-11 21:46:01 +0800

    完成正金字塔的输出代码

---
+++ PyramidDemo.java
@@ -0,0 +1,24 @@
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/esllovelxz/test.git
+git push -u origin master
